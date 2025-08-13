638c8e09c05fcb442d08667319b4e4f5407edd77
okoro91 <mi.fracri91@gmail.com>
2025-08-13 18:56:27 +0100

    connected server to mongoDB

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,31 @@
-import express from "express";
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app: Express = express();
+const PORT: number = parseInt(process.env.PORT as string, 10) || 5000;
+const MONGO_URI: string =
+  "mongodb+srv://tradelink_admin:Sail53Group@tradelink.i7nexqe.mongodb.net/?retryWrites=true&w=majority&appName=Tradelink";
+
+app.use(cors());
+app.use(express.json());
+app.use("/uploads", express.static("uploads"));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to TradeLink Backend server");
+});
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB successfully");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
